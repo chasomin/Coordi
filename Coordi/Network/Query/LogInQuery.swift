0cc6f9a54f6ccce6daddf3dd2328715a4cc0ce29
chasomin <somin49@naver.com>
2024-04-13 23:18:45 +0900

    Feat: 로그인 요청 Query

---
+++ Coordi/Network/Query/LogInQuery.swift
@@ -0,0 +1,13 @@
+//
+//  LogInQuery.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/12/24.
+//
+
+import Foundation
+ 
+struct LogInQuery: Encodable {
+    let email: String
+    let password: String
+}

1bcab0127cb46b05ad49f51f6009891e2fc92299
chasomin <somin49@naver.com>
2024-04-13 23:19:18 +0900

    Feat: 프로필 수정 요청 Query

---
+++ Coordi/Network/Query/ProfileQuery.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileQuery.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/13/24.
+//
+
+import Foundation
+
+struct ProfileQuery: Encodable {
+    let nick: String
+    let profile: Data
+}

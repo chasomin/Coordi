c8a387ff5cbc98c369aef8d70927e4ee4ae4fcdc
chasomin <somin49@naver.com>
2024-04-13 23:37:06 +0900

    Feat: 회원탈퇴 응답 Model

---
+++ Coordi/Network/Model/WithdrawModel.swift
@@ -0,0 +1,14 @@
+//
+//  WithdrawModel.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/13/24.
+//
+
+import Foundation
+
+struct WithdrawModel: Decodable {
+    let user_id: String
+    let email: String
+    let nick: String
+}

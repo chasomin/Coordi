efe5b7bc321f158954179c6d4a5d828cd64a98f8
chasomin <somin49@naver.com>
2024-04-15 16:49:55 +0900

    Feat: Post GET 파라미터 Query

---
+++ Coordi/Network/Query/FetchPostQuery.swift
@@ -0,0 +1,15 @@
+//
+//  FetchPostQuery.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import Foundation
+
+struct FetchPostQuery {
+    let next: String = ""
+    let limit: String = ""
+    let product_id: String = ""
+    let hashTag: String
+}

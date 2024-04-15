3745f0c42a4cbf929d6e16431ad3fd44a3c430ca
chasomin <somin49@naver.com>
2024-04-15 18:58:18 +0900

    Feat: 언더 라인이 있는 TextFieldView 구현

---
+++ Coordi/Custom/LineTextField.swift
@@ -0,0 +1,32 @@
+//
+//  LineTextField.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import UIKit
+import SnapKit
+
+final class LineTextField: BaseView {
+    let textField = UITextField()
+    let line = UIView()
+    
+    override func configureHierarchy() {
+        addSubview(textField)
+        addSubview(line)
+    }
+    
+    override func configureLayout() {
+        textField.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview()
+            make.height.equalTo(28)
+        }
+        
+        line.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(5)
+            make.horizontalEdges.equalTo(textField)
+            make.height.equalTo(1)
+        }
+    }
+}

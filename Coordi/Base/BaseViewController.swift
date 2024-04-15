684cf79552b820e7c0951880311a99b492324a9c
chasomin <somin49@naver.com>
2024-04-15 17:23:03 +0900

    Feat: 모든 ViewController에서 상속받을 BaseViewController 구현

---
+++ Coordi/Base/BaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class BaseViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.backgroundColor
+        bind()
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func bind() { }
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+
+    @available (*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

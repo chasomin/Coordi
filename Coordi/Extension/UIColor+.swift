91a60619bcf98911349407bb18334682ad49514e
chasomin <somin49@naver.com>
2024-04-15 17:22:19 +0900

    Feat: 사용할 컬러 UIColor Extension 구현

---
+++ Coordi/Extension/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor.white
+    static let LabelColor = UIColor.black
+    static let pointColor = UIColor.init(red: 210/255, green: 113/255, blue: 119/255, alpha: 1)
+}

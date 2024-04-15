bb67e2a11cb85ec2259b0f77a571a7d0c06c4024
chasomin <somin49@naver.com>
2024-04-16 01:15:07 +0900

    Feat: 사용할 폰트 사이즈 추가

--- Coordi/Extension/UIFont+.swift
+++ Coordi/Extension/UIFont+.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIFont {
     static let boldTitle = UIFont.boldSystemFont(ofSize: 18)
     static let title = UIFont.systemFont(ofSize: 18)
-    static let boidBody = UIFont.boldSystemFont(ofSize: 14)
-    static let body = UIFont.systemFont(ofSize: 14)
+    static let body = UIFont.systemFont(ofSize: 15)
+    static let boldBody = UIFont.boldSystemFont(ofSize: 15)
+    static let caption = UIFont.systemFont(ofSize: 13)
 }

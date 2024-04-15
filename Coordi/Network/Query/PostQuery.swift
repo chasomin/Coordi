f888feca4ee54ccde211edd5a44f182af41e75e8
chasomin <somin49@naver.com>
2024-04-15 16:50:25 +0900

    Comment: 주석 추가

--- Coordi/Network/Query/PostQuery.swift
+++ Coordi/Network/Query/PostQuery.swift
@@ -12,6 +12,6 @@ struct PostQuery: Encodable {
     let content: String     // 해시태그
     let content1: String    // 본문
     let content2: String    // 가격
-    let product_id: String  // 피드인지 상품화면인지 구분
+    let product_id: String  // 피드인지 상품화면인지 구분, 해시태그 등 검색 시 다른 서버 값 포함 안되도록 구분
     let files: [String]     // 사진
 }

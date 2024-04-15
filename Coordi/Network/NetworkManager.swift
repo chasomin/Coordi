0d15dedc9f6d27469baa99c8b9c1fb6b87a8b919
chasomin <somin49@naver.com>
2024-04-16 01:11:52 +0900

    Feat: NetworkManager API 요청 타입메서드 구현

---
+++ Coordi/Network/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkManager.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+struct NetworkManager {
+    static func request<T: Decodable>(api: Router) -> Single<T> {
+        return Single<T>.create { single in
+            do {
+                let urlRequest = try api.asURLRequest()
+                                
+                AF.request(urlRequest)
+                    .responseDecodable(of: T.self) { response in
+                        switch response.result {
+                        case .success(let success):
+                            single(.success(success))
+                        case .failure(let error):
+                            single(.failure(error))
+                            print(error, response.response?.statusCode)
+                        }
+                    }
+            } catch {
+                single(.failure(error))
+            }
+            return Disposables.create()
+        }
+    }
+}

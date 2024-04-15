1d1a31c9b34ccbadf1509deac30be06c77e6ab32
chasomin <somin49@naver.com>
2024-04-15 18:57:17 +0900

    Feat: 상황에 따른 폰트 사이즈 구현

---
+++ Coordi/Extension/UIFont+.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/15/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let boldTitle = UIFont.boldSystemFont(ofSize: 18)
+    static let title = UIFont.systemFont(ofSize: 18)
+    static let boidBody = UIFont.boldSystemFont(ofSize: 14)
+    static let body = UIFont.systemFont(ofSize: 14)
+}

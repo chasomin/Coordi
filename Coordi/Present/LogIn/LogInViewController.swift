ee427b5e537f18aada7a75a06cc96f6aa9d87777
chasomin <somin49@naver.com>
2024-04-16 18:09:19 +0900

    Feat: 로그인 구현(API 통신해서 응답값 Token과 user Id UserDefaults 저장)

---
+++ Coordi/Present/LogIn/LogInViewController.swift
@@ -0,0 +1,151 @@
+//
+//  LogInViewController.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/16/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class LogInViewController: BaseViewController {
+    let viewModel = LogInViewModel()
+    
+    let logoImageView = UIImageView()
+        
+    let emailLabel = UILabel()
+    let emailTextField = LineTextField()
+    
+    let passwordLabel = UILabel()
+    let passwordTextField = LineTextField()
+        
+    let logInButton = PointButton(text: "로그인")
+    
+    let moveSignUpButton = UIButton()
+    
+    let tapGesture = UITapGestureRecognizer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func bind() {
+        let input = LogInViewModel.Input(emailText: emailTextField.textField.rx.text.orEmpty.asObservable(), passwordText: passwordTextField.textField.rx.text.orEmpty.asObservable(), logInButtonTap: logInButton.rx.tap.asObservable(), moveSignUpButtonTap: moveSignUpButton.rx.tap.asObservable())
+        let output = viewModel.transform(input: input)
+        
+        output.logInButtonStatus
+            .drive(logInButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        output.successTrigger
+            .drive(with: self) { owner, _ in
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                
+                let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                
+                let vc = ViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                
+                sceneDelegate?.window?.rootViewController = nav
+                sceneDelegate?.window?.makeKeyAndVisible()
+            }
+            .disposed(by: disposeBag)
+        
+        output.failureTrigger
+            .drive(with: self) { owner, _ in
+                owner.showToast()
+            }
+            .disposed(by: disposeBag)
+        
+        output.moveSignUp
+            .drive(with: self) { owner, _ in
+                let vc = SignUpViewController()
+                vc.modalPresentationStyle = .fullScreen
+                owner.present(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(logoImageView)
+        view.addSubview(emailLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordLabel)
+        view.addSubview(passwordTextField)
+        view.addSubview(logInButton)
+        view.addSubview(moveSignUpButton)
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    override func configureLayout() {
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.height.equalTo(100)
+        }
+        
+        emailLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.height.equalTo(24)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.height.equalTo(28)
+        }
+
+        passwordLabel.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(25)
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.height.equalTo(24)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.height.equalTo(28)
+        }
+        
+        logInButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view).inset(15)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(50)
+        }
+        
+        moveSignUpButton.snp.makeConstraints { make in
+            make.bottom.equalTo(logInButton.snp.top).offset(-10)
+            make.horizontalEdges.equalTo(view).inset(15)
+        }
+    }
+    
+    override func configureView() {
+        logoImageView.image = .coordi
+        logoImageView.contentMode = .scaleAspectFit
+        
+        emailLabel.text = "이메일"
+        emailLabel.font = .title
+        emailTextField.textField.placeholder = "ex) coordi@coordi.com"
+        
+        passwordLabel.text = "비밀번호"
+        passwordTextField.textField.isSecureTextEntry = true
+        
+        var config = UIButton.Configuration.plain()
+        var attr = AttributedString.init("아직 회원이 아니신가요?   이메일 가입")
+        attr.font = UIFont.caption
+        config.attributedSubtitle = attr
+        config.baseForegroundColor = .LabelColor
+        moveSignUpButton.configuration = config
+
+        tapGesture.addTarget(self, action: #selector(tapGestureTapped))
+    }
+}
+
+extension LogInViewController {
+    @objc func tapGestureTapped() {
+        view.endEditing(true)
+    }
+}
---
+++ Coordi/Present/LogIn/LogInViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  LogInViewModel.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/16/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class LogInViewModel: ViewModelType {
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        let emailText: Observable<String>
+        let passwordText: Observable<String>
+        let logInButtonTap: Observable<Void>
+        let moveSignUpButtonTap: Observable<Void>
+    }
+    
+    struct Output {
+        let logInButtonStatus: Driver<Bool>
+        let successTrigger: Driver<Void>
+        let failureTrigger: Driver<Void>
+        let moveSignUp: Driver<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let logInButtonStatus = BehaviorRelay(value: false)
+        let successTrigger = PublishRelay<Void>()
+        let failureTrigger = PublishRelay<Void>()
+        
+        Observable.combineLatest(input.emailText, input.passwordText)
+            .map {
+                !$0.0.isEmpty && !$0.1.isEmpty
+            }
+            .subscribe { valid in
+                logInButtonStatus.accept(valid)
+            }
+            .disposed(by: disposeBag)
+        
+        let logInQuery = Observable.combineLatest(input.emailText, input.passwordText)
+            .map { email, password in
+                return LogInQuery(email: email, password: password)
+            }
+        
+        input.logInButtonTap
+            .throttle(.seconds(2), scheduler: MainScheduler.instance)
+            .withLatestFrom(logInQuery)
+            .flatMap { logInQuery in
+                NetworkManager.request(api: .logIn(query: logInQuery))
+                    .catch { _ in
+                        failureTrigger.accept(())
+                        return Single<LogInModel>.never()
+                    }
+            }
+            .subscribe { loginModel in
+                successTrigger.accept(())
+                UserDefaults.standard.set(loginModel.element?.accessToken, forKey: "accessToken")
+                UserDefaults.standard.set(loginModel.element?.refreshToken, forKey: "refreshToken")
+                UserDefaults.standard.set(loginModel.element?.user_id, forKey: "user_id")
+            }
+            .disposed(by: disposeBag)
+
+        
+        return Output.init(logInButtonStatus: logInButtonStatus.asDriver(onErrorJustReturn: false), successTrigger: successTrigger.asDriver(onErrorJustReturn: ()), failureTrigger: failureTrigger.asDriver(onErrorJustReturn: ()), moveSignUp: input.moveSignUpButtonTap.asDriver(onErrorJustReturn: ()))
+    }
+}

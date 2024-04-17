39eea601716b46adcea143dc3f32be1485075276
chasomin <somin49@naver.com>
2024-04-17 19:04:53 +0900

    Feat: UICollectionViewCell extension 메서드로 타입 프로퍼티 id 구현

---
+++ Coordi/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/17/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var id: String {
+        self.description()
+    }
+}

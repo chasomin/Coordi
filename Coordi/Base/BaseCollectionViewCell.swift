464c7dff3df5bb4a8465809620230229f7aa5678
chasomin <somin49@naver.com>
2024-04-17 19:04:13 +0900

    Feat: Base CollectionViewCell 생성

---
+++ Coordi/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseCollectionViewCell.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/17/24.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+}

6ce868002b331ec83871198b16c043cf8831c4b5
chasomin <somin49@naver.com>
2024-04-18 19:24:50 +0900

    Feat: UserDefaults PropertyWrapper 구현

--- Coordi/UserDefaults/CoordiUserDefaults.swift
+++ Coordi/UserDefaults/CoordiUserDefaults.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct CoordiUserDefaults<T> {
+    let key: String
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        get {
+            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}

3be95ed6aadcb99315a71bbfa7ae43e4e43b51dc
chasomin <somin49@naver.com>
2024-04-18 19:25:58 +0900

    Feat: PropertyWrapper 사용하여 UserDefaults Manager 구현

--- Coordi/UserDefaults/UserDefaultsManager.swift
+++ Coordi/UserDefaults/UserDefaultsManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum UserDefaultsManager {
+    enum Key: String {
+        case user_id, accessToken, refreshToken
+    }
+    
+    @CoordiUserDefaults(key: Key.user_id.rawValue, defaultValue: "")
+    static var userId
+    
+    @CoordiUserDefaults(key: Key.accessToken.rawValue, defaultValue: "")
+    static var accessToken
+    
+    @CoordiUserDefaults(key: Key.refreshToken.rawValue, defaultValue: "")
+    static var refreshToken
+}

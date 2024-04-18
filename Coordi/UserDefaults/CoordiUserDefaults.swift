312d41b6219eeca9efc739c04a087c2489128415
chasomin <somin49@naver.com>
2024-04-18 19:18:35 +0900

    Feat: UserDefaults 관련 폴더 구성

---
+++ Coordi/UserDefaults/CoordiUserDefaults.swift
@@ -0,0 +1,8 @@
+//
+//  CoordiUserDefaults.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/18/24.
+//
+
+import Foundation
---
+++ Coordi/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsManager.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/18/24.
+//
+
+import Foundation

dc293a8ceb060316ceabe48b3b4ed544cf84fcf4
chasomin <somin49@naver.com>
2024-04-20 00:25:22 +0900

    Feat: 리터럴 값 관리할 enum 생성

---
+++ Coordi/Constants/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/19/24.
+//
+
+import Foundation
+
+enum Constants {
+    enum productId: String {
+        case id = "CoordiFeed"
+    }
+    
+    enum NavigationTitle {
+        case feed
+        case myPage
+        case search
+        case create
+    }
+}

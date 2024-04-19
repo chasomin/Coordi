ea8f381bcb69dc58b4d2ae237566ff82ee5c1890
chasomin <somin49@naver.com>
2024-04-20 00:19:15 +0900

    Feat: Image Data upload 하는 메서드 구현

--- Coordi/Network/NetworkManager.swift
+++ Coordi/Network/NetworkManager.swift
@@ -9,13 +9,21 @@ import Foundation
 import Alamofire
 import RxSwift
 
+class API {
+    static let session: Session = {
+        let configuration = URLSessionConfiguration.af.default
+        let apiLogger = APIMonitor()
+        return Session(configuration: configuration, eventMonitors: [apiLogger])
+    }()
+}
+
 struct NetworkManager {
     static func request<T: Decodable>(api: Router) -> Single<T> {
         return Single<T>.create { single in
             do {
                 let urlRequest = try api.asURLRequest()
-                                
-                AF.request(urlRequest, interceptor: TokenRefresh())
+                
+                API.session.request(urlRequest, interceptor: TokenRefresh())
                     .responseDecodable(of: T.self) { response in
                         switch response.result {
                         case .success(let success):
@@ -28,6 +36,41 @@ struct NetworkManager {
                     }
             } catch {
                 single(.failure(error))
+            }
+            return Disposables.create()
+        }
+    }
+    
+    static func upload(api: Router, images: [Data]) -> Single<ImageUploadModel> {
+        return Single<ImageUploadModel>.create { single in
+            guard let url = URL(string: api.baseURL + api.path) else { return Disposables.create() }
+            let accessToken = UserDefaultsManager.accessToken
+            print(url)
+            let headers: HTTPHeaders = [
+                HTTPHeader.sesacKey.rawValue: APIKey.key.rawValue,
+                HTTPHeader.contentType.rawValue: HTTPHeader.multi.rawValue,
+                HTTPHeader.authorization.rawValue: accessToken
+            ]
+            
+            AF.upload(multipartFormData: { multipartFormData in
+                for image in images {
+                    multipartFormData.append(image,
+                                             withName: "files",
+                                             fileName: "Coordi.jpg",
+                                             mimeType: "image/jpeg")
+                }
+                
+            }, to: url, method: .post, headers: headers)
+            .responseDecodable(of: ImageUploadModel.self) { response in
+                switch response.result {
+                case .success(let success):
+                    single(.success(success))
+                    dump(success)
+                    
+                case .failure(let error):
+                    single(.failure(error))
+                    print(error, response.response?.statusCode)
+                }
             }
             return Disposables.create()
         }

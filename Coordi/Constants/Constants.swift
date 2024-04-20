0496da4217bd05308669a4455452c71987dcf24b
chasomin <somin49@naver.com>
2024-04-21 07:25:41 +0900

    Feat: NavigaionTitle 추가, TextView Placeholder로 쓰일 String 추가

--- Coordi/Constants/Constants.swift
+++ Coordi/Constants/Constants.swift
@@ -17,5 +17,33 @@ enum Constants {
         case myPage
         case search
         case create
+        case editProfile
+        case editNickname
+        
+        var title: String {
+            switch self {
+            case .feed:
+                ""
+            case .myPage:
+                "내 피드 모아보기"
+            case .search:
+                ""
+            case .create:
+                "코디 올리기"
+            case .editProfile:
+                "프로필 관리"
+            case .editNickname:
+                "닉네임 수정"
+            }
+        }
+    }
+    
+    enum Temp: CaseIterable {
+        
+        
+    }
+    
+    enum TextViewPlaceholder: String {
+        case createPost = "아이템 정보와 코디에 대해 알려주세요!"
     }
 }

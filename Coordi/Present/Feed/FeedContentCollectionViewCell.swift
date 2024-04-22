aa8a9e3c67f9d7745f5c0fd3cf1ea9a47f02e416
chasomin <somin49@naver.com>
2024-04-22 23:03:15 +0900

    Feat: 게시글 Detail 화면에서 사용될 컨텐츠 Cell 구현(게시글 내용, 해시태그, 좋아요, 게시일)

---
+++ Coordi/Present/Feed/FeedContentCollectionViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  FeedContentView.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/21/24.
+//
+
+import UIKit
+import SnapKit
+
+final class FeedContentCollectionViewCell: BaseCollectionViewCell {
+    let tempLabel = UILabel()
+    let heartButton = UIButton()
+    let contentLabel = UILabel()
+    let dateLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(tempLabel)
+        contentView.addSubview(heartButton)
+        contentView.addSubview(contentLabel)
+        contentView.addSubview(dateLabel)
+    }
+    override func configureLayout() {
+        tempLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        heartButton.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(tempLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalToSuperview()
+            make.height.greaterThanOrEqualTo(24)
+
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(10)
+            make.horizontalEdges.bottom.equalToSuperview()
+            make.height.equalTo(24)
+        }
+    }
+    override func configureView() {
+        tempLabel.font = .body
+        heartButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        contentLabel.font = .body
+        contentLabel.numberOfLines = 0
+        dateLabel.textColor = .gray
+        dateLabel.font = .caption
+    }
+}

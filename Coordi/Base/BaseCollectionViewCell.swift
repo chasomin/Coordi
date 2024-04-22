f8e53f6ef2986fe8b8f79e3be3d057e1bab053c6
chasomin <somin49@naver.com>
2024-04-22 23:09:31 +0900

    Design: CollectionViewCell backgroundColor 추가

--- Coordi/Base/BaseCollectionViewCell.swift
+++ Coordi/Base/BaseCollectionViewCell.swift
@@ -14,6 +14,7 @@ class BaseCollectionViewCell: UICollectionViewCell {
         configureHierarchy()
         configureLayout()
         configureView()
+        contentView.backgroundColor = .backgroundColor
     }
     
     @available(*, unavailable)

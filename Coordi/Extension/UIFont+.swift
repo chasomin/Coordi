5cb513642bf05bdd5f7643183a6ef0c2d1fc2748
chasomin <somin49@naver.com>
2024-04-24 04:25:24 +0900

    Design: Custom Font 추가 구현

--- Coordi/Extension/UIFont+.swift
+++ Coordi/Extension/UIFont+.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UIFont {
+    static let boldLargeTitle = UIFont.boldSystemFont(ofSize: 30) 
+    static let largeTitle = UIFont.systemFont(ofSize: 30)
     static let boldTitle = UIFont.boldSystemFont(ofSize: 18)
     static let title = UIFont.systemFont(ofSize: 18)
     static let body = UIFont.systemFont(ofSize: 15)

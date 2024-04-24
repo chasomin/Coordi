f029390e0cb863c51763b6ebc5b2edbd0a9ad0cf
chasomin <somin49@naver.com>
2024-04-25 00:23:17 +0900

    Feat: 상태코드 정리

--- Coordi/Network/NetworkManager.swift
+++ Coordi/Network/NetworkManager.swift
@@ -29,13 +29,40 @@ struct NetworkManager {
                         case .success(let success):
                             single(.success(success))
                             dump(success)
-                        case .failure(let error):
-                            single(.failure(error))
-                            print(error, response.response?.statusCode)
+                        case .failure(_):
+                            guard let statusCode = response.response?.statusCode else { return }
+                            switch statusCode {
+                            case 400:
+                                single(.failure(CoordiError.invalidRequest(kind: api)))
+                            case 401:
+                                single(.failure(CoordiError.unauthenticatedToken(kind: api)))
+                            case 403:
+                                single(.failure(CoordiError.forbidden))
+                            case 409:
+                                single(.failure(CoordiError.alreadySubscribed))
+                            case 410:
+                                single(.failure(CoordiError.creationFailed))
+                            case 418:
+                                single(.failure(CoordiError.refreshTokenExpired))
+                            case 419:
+                                single(.failure(CoordiError.accessTokenExpired))
+                            case 420:
+                                single(.failure(CoordiError.withoutKey))
+                            case 429:
+                                single(.failure(CoordiError.overCall))
+                            case 444:
+                                single(.failure(CoordiError.invalidURL))
+                            case 445:
+                                single(.failure(CoordiError.haveNoAuthority))
+                            case 500:
+                                single(.failure(CoordiError.unknownError))
+                            default:
+                                single(.failure(CoordiError.unknownError))
+                            }
                         }
                     }
             } catch {
-                single(.failure(error))
+                single(.failure(CoordiError.unknownError))
             }
             return Disposables.create()
         }
@@ -80,12 +107,113 @@ struct NetworkManager {
                     single(.success(success))
                     dump(success)
                     
-                case .failure(let error):
-                    single(.failure(error))
-                    print(error, response.response?.statusCode)
+                case .failure(_):
+                    guard let statusCode = response.response?.statusCode else { return }
+                    switch statusCode {
+                    case 400:
+                        single(.failure(CoordiError.invalidRequest(kind: api)))
+                    case 401:
+                        single(.failure(CoordiError.unauthenticatedToken(kind: api)))
+                    case 403:
+                        single(.failure(CoordiError.forbidden))
+                    case 409:
+                        single(.failure(CoordiError.alreadySubscribed))
+                    case 410:
+                        single(.failure(CoordiError.creationFailed))
+                    case 418:
+                        single(.failure(CoordiError.refreshTokenExpired))
+                    case 419:
+                        single(.failure(CoordiError.accessTokenExpired))
+                    case 420:
+                        single(.failure(CoordiError.withoutKey))
+                    case 429:
+                        single(.failure(CoordiError.overCall))
+                    case 444:
+                        single(.failure(CoordiError.invalidURL))
+                    case 445:
+                        single(.failure(CoordiError.haveNoAuthority))
+                    case 500:
+                        single(.failure(CoordiError.unknownError))
+                    default:
+                        single(.failure(CoordiError.unknownError))
+                    }
                 }
             }
             return Disposables.create()
+        }
+    }
+}
+
+
+enum CoordiError: /*String,*/ Error {
+    case withoutKey// = "SeSAC Memolease Only"
+    
+//    case noRequiredValue// = "필수 값을 채워주세요"
+    case invalidRequest(kind: Router)// = "잘못된 요청"
+    
+//    case checkAccount// = "계정을 확인해주세요"
+    case unauthenticatedToken(kind: Router)// = "인증할 수 없는 토큰"
+    
+    case forbidden //= "접근권한 없음"
+    
+    case alreadySubscribed// = "이미 가입된 계정"
+    
+    case creationFailed// = "생성 실페"
+    
+    case refreshTokenExpired //= "refresh토큰 만료 다시 로그인"
+    
+    case accessTokenExpired// = "accessToken 만료"
+    
+    case overCall //= "과호출"
+    
+    case invalidURL// = "비정상 URL"
+    
+    case haveNoAuthority// = "다른 사람의 게시글,댓글에 접근"//445
+    
+    case unknownError// = "알 수 없는 오류"
+    
+    var errorMessage: String {
+        switch self {
+        case .withoutKey:                       // 420
+            "SeSAC Memolease Only"
+        case .invalidRequest(let kind):         // 400
+            switch kind {
+            case .signUp, .emailValidation, .logIn:
+                "필수 값을 채워주세요"
+            case .uploadImage, .editProfileImage:
+                "이미지 용량이 커요"
+            case .uploadComment, .editComment:
+                "댓글 내용을 채워주세요"
+            case .like, .follow, .hashtag, .fetchPost, .fetchParticularPost, .fetchPostByUser, .fetchLikePost:
+                "잘못된 요청"
+            default:
+                ""
+            }
+        case .unauthenticatedToken(let kind):   // 401
+            switch kind {
+            case .logIn(let query):
+                "계정을 확인해주세요"
+            default:
+                "인증할 수 없는 토큰"
+            }
+        case .forbidden:                        // 403
+            "접근권한 없음"
+        case .alreadySubscribed:                // 409
+            "이미 가입된 계정"
+        case .creationFailed:                   // 410, 저장수정삭제
+            "생성 실패"
+        case .refreshTokenExpired:              // 418
+            "refresh토큰 만료 다시 로그인"
+        case .accessTokenExpired:               // 419
+            "accessToken 만료"
+        case .overCall:                         // 429
+            "과호출"
+        case .invalidURL:                       // 444
+            "비정상 URL"
+        case .haveNoAuthority:                  // 445
+            "다른 사람의 게시글,댓글에 접근"
+        case .unknownError:                     // 500
+            "오류가 발생했어요"
         }
     }
 }

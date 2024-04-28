ed5a74b69d9ce55b06d385fc8f1b95e772ad3ea7
chasomin <somin49@naver.com>
2024-04-29 02:02:26 +0900

    Feat: 게시글 디테일 화면에 게시글 수정, 삭제 버튼 구성

--- Coordi/Present/Feed/Detail/Cell/ProfileNavigationCollectionViewCell.swift
+++ Coordi/Present/Feed/Detail/Cell/ProfileNavigationCollectionViewCell.swift
@@ -16,6 +16,11 @@ final class ProfileNavigationCollectionViewCell: BaseCollectionViewCell {
     let nicknameLabel = UILabel()
     let backButton = UIButton()
     let tapGesture = UITapGestureRecognizer()
+    let editButton = UIButton()
+    
+//    let editAction = UIAction(title: "삭제하기", handler: { _ in })//
+//    let deleteAction = UIAction(title: "삭제하기", attributes: .destructive, handler: { _ in print("확인") })//
+
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -27,6 +32,7 @@ final class ProfileNavigationCollectionViewCell: BaseCollectionViewCell {
         contentView.addSubview(nicknameLabel)
         contentView.addSubview(backButton)
         profileImage.addGestureRecognizer(tapGesture)
+        contentView.addSubview(editButton)
     }
     
     override func configureLayout() {
@@ -43,6 +49,10 @@ final class ProfileNavigationCollectionViewCell: BaseCollectionViewCell {
             make.centerX.equalToSuperview()
             make.height.equalTo(20)
         }
+        editButton.snp.makeConstraints { make in
+            make.top.equalTo(backButton)
+            make.trailing.equalToSuperview().inset(10)
+        }
     }
     
     override func configureView() {
@@ -53,10 +63,20 @@ final class ProfileNavigationCollectionViewCell: BaseCollectionViewCell {
         profileImage.backgroundColor = .pointColor
         profileImage.isUserInteractionEnabled = true
         nicknameLabel.font = .caption
+        
+        let editImage = UIImage(systemName: "ellipsis")?.setConfiguration(font: .boldSystemFont(ofSize: 22))
+        editButton.setImage(editImage,for: .normal)
+        editButton.showsMenuAsPrimaryAction = true
+//        let buttonMenu = UIMenu(title: "", children: [edit, delete])
+//        editButton.menu = buttonMenu
     }
     
-    func configureCell(item: UserModel) {
+    func configureCell(item: UserModel, postModel: PostModel) {
         nicknameLabel.text = item.nick
         profileImage.loadImage(from: item.profileImage)
+        
+        if postModel.creator.user_id != UserDefaultsManager.userId {
+            editButton.isHidden = true
+        }
     }
 }

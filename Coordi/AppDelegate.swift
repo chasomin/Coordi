44d84eb90cce34079255a27b7522b466cf9d12c2
chasomin <somin49@naver.com>
2024-04-30 03:09:58 +0900

    Chore: IQKeyboard 라이브러리 세팅

--- Coordi/AppDelegate.swift
+++ Coordi/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.disabledDistanceHandlingClasses = [CommentViewController.self]
+        IQKeyboardManager.shared.disabledToolbarClasses = [CommentViewController.self]
         return true
     }
 

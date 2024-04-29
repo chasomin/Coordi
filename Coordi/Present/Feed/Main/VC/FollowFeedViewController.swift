989d85ab4c658908e2ba04e8f035093b039d3c3b
chasomin <somin49@naver.com>
2024-04-29 21:59:21 +0900

    Feat: 기온에 맞는 팔로잉 게시글 화면 구성

---
+++ Coordi/Present/Feed/Main/VC/FollowFeedViewController.swift
@@ -0,0 +1,92 @@
+//
+//  FollowFeedViewController.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/29/24.
+//
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+import Alamofire
+
+final class FollowFeedViewController: BaseViewController {
+    private let viewModel: FollowFeedViewModel
+    
+    private let viewReloadTrigger = PublishRelay<Void>()
+    private let postData = BehaviorRelay<[PostModel]>(value: [])
+
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+
+    init(viewModel: FollowFeedViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viewReloadTrigger.accept(())
+    }
+    
+    override func bind() {
+        let input = FollowFeedViewModel.Input(dataReload: viewReloadTrigger,
+                                              itemSelected: collectionView.rx.modelSelected(PostModel.self).asObservable())
+        let output = viewModel.transform(input: input)
+        
+        output.postData
+            .drive(collectionView.rx.items(cellIdentifier: FollowingFeedCollectionViewCell.id, cellType: FollowingFeedCollectionViewCell.self)) { index, element, cell in
+                cell.configureCell(item: element)
+                let imageView = UIImageView()
+                imageView.loadImage(from: element.files.first!)
+            }
+            .disposed(by: disposeBag)
+        
+        output.requestFailureTrigger
+            .drive(with: self) { owner, errorText in
+                owner.showErrorToast(errorText)
+            }
+            .disposed(by: disposeBag)
+        
+        output.itemSelected
+            .drive(with: self) { owner, postModel in
+                owner.navigationController?.pushViewController(FeedDetailViewController(postModel: BehaviorRelay(value: postModel)), animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(collectionView)
+    }
+    
+    override func configureLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        collectionView.register(FollowingFeedCollectionViewCell.self, forCellWithReuseIdentifier: FollowingFeedCollectionViewCell.id)
+    }
+}
+
+extension FollowFeedViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 15
+        layout.itemSize = CGSize(width: (view.frame.width - spacing * 2) , height: ((view.frame.width - spacing * 2)) * 1.4)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.scrollDirection = .vertical
+        return layout
+    }
+}
---
+++ Coordi/Present/Feed/Main/VC/FollowFeedViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  FeedViewModel.swift
+//  Coordi
+//
+//  Created by 차소민 on 4/25/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class FollowFeedViewModel: ViewModelType {
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        let dataReload: PublishRelay<Void> // 넘길 때 next랑, hashtag 넘기면 되나..?
+        let itemSelected: Observable<PostModel>
+    }
+    
+    struct Output {
+        let postData: Driver<[PostModel]>
+        let requestFailureTrigger: Driver<String>
+        let itemSelected: Driver<PostModel>
+    }
+    
+    func transform(input: Input) -> Output {
+        let postData = PublishRelay<[PostModel]>()
+        let requestFailureTrigger = PublishRelay<String>()
+
+        input.dataReload
+            .flatMap { _ in
+                let posts: PrimitiveSequence<SingleTrait, PostListModel> = NetworkManager.request(api: .hashtag(query: FetchPostQuery(next: "", limit: "", product_id: Constants.productId, hashTag: "20")))
+                    .catch { error in
+                        let coordiError = error as! CoordiError
+                        requestFailureTrigger.accept(coordiError.errorMessage)
+                        print("!@!@!@", coordiError.errorMessage)
+                        return Single<PostListModel>.never()
+                    }
+                return posts
+            }
+            //TODO: 팔로워 게시글만 ..
+//            .flatMap { posts in
+//                let profile: PrimitiveSequence<SingleTrait, ProfileModel> = NetworkManager.request(api: .fetchMyProfile)
+//                    .catch { error in
+//                        
+//                        return Single<ProfileModel>.never()
+//                    }
+//                return posts.data.map { post in
+//                    profile.map { profile in
+//                        profile.following.map {
+//                            $0.user_id == post.creator.user_id
+//                        }
+//                        }
+//                }
+//                
+////                let followPosts: PostListModel = posts.data.map { post in
+////                    profile.map { profile in
+////                        profile.following.map { $0.user_id == post.creator.user_id}
+////                    }
+////                }
+//                
+//
+//            }
+            .subscribe(onNext: { posts in
+                postData.accept(posts.data)
+            })
+            .disposed(by: disposeBag)
+            
+        
+        
+        
+        
+        return Output.init(postData: postData.asDriver(onErrorJustReturn: []),
+                           requestFailureTrigger: requestFailureTrigger.asDriver(onErrorJustReturn: ""),
+                           itemSelected: input.itemSelected.asDriver(onErrorJustReturn: .dummy))
+    }
+}

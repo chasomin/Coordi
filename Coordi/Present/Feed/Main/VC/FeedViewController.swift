a8e8614b642d6bd73d75f2dda38436a7c700dd0b
chasomin <somin49@naver.com>
2024-04-30 21:54:34 +0900

    Feat: 현재 위치와 날씨 받아와서 화면 적용

--- Coordi/Present/Feed/Main/VC/FeedViewController.swift
+++ Coordi/Present/Feed/Main/VC/FeedViewController.swift
@@ -9,13 +9,20 @@ import Tabman
 import Pageboy
 import RxSwift
 import RxCocoa
+import WeatherKit
+import CoreLocation
 
 final class FeedViewController: TabmanViewController {
     private let viewModel: FeedViewModel
     private let disposeBag = DisposeBag()
     
+    private let temp = PublishRelay<Double>()
+    
     private var viewControllers: [UIViewController]
     private let searchButton = UIBarButtonItem()
+    
+    private let weatherService = WeatherService()
+    private let locationManager = CLLocationManager()
     
     init(viewModel: FeedViewModel, followFeedViewModel: FollowFeedViewModel, allFeedViewModel: AllFeedViewModel) {
         self.viewModel = viewModel
@@ -28,14 +35,35 @@ final class FeedViewController: TabmanViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setTopTabBar()
         configureView()
         bind()
-        navigationItem.title = "현재 20℃"//
         navigationItem.rightBarButtonItem = searchButton
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        requestLocationPermission()
+
+    }
+    
+    private func bind() {
+        let input = FeedViewModel.Input(searchButtonTap: searchButton.rx.tap.asObservable(),
+                                        temp: temp)
+        let output = viewModel.transform(input: input)
         
+        output.serarchButtonTap
+            .drive(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(SearchViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        output.tempText
+            .drive(with: self) { owner, text in
+                owner.navigationItem.title = text
+            }
+            .disposed(by: disposeBag)
     }
     
     private func setTopTabBar() {
@@ -53,19 +81,10 @@ final class FeedViewController: TabmanViewController {
     
     private func configureView() {
         searchButton.image = UIImage(systemName: "magnifyingglass")
-
+        navigationItem.title = "현재 기온 알 수 없음"   // 기온 모를 때는 전체 보여주기 OR 텅뷰 만들어서 위치허용 유도하기
     }
     
-    private func bind() {
-        let input = FeedViewModel.Input(searchButtonTap: searchButton.rx.tap.asObservable())
-        let output = viewModel.transform(input: input)
-        
-        output.serarchButtonTap
-            .drive(with: self) { owner, _ in
-                owner.navigationController?.pushViewController(SearchViewController(), animated: true)
-            }
-            .disposed(by: disposeBag)
-    }
+    
 }
 
 extension FeedViewController: PageboyViewControllerDataSource, TMBarDataSource {
@@ -88,4 +107,52 @@ extension FeedViewController: PageboyViewControllerDataSource, TMBarDataSource {
     func defaultPage(for pageboyViewController: Pageboy.PageboyViewController) -> Pageboy.PageboyViewController.Page? {
         nil
     }
+}
+
+extension FeedViewController {
+    func getWeather(location: CLLocation) {
+        Task {
+            do {
+                let weather = try await WeatherService.shared.weather(for: location)
+                print("Temp: \(weather.currentWeather.temperature)")
+                temp.accept(weather.currentWeather.temperature.value)
+                
+            } catch {
+                print("날씨 못 받아옴",String(describing: error.localizedDescription))
+            }
+        }
+    }
+}
+
+extension FeedViewController: CLLocationManagerDelegate {
+    
+    func requestLocationPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("Latitude: \(location.coordinate.latitude), Longitude: \(location.coordinate.longitude)")
+            getWeather(location: location)
+
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Error: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("GPS 권한 설정됨")
+            locationManager.startUpdatingLocation()
+        case .restricted, .notDetermined:
+            print("GPS 권한 설정되지 않음")
+        case .denied:
+            print("GPS 권한 요청 거부됨")
+        default:
+            print("GPS: Default")
+        }
+    }
+    
 }
--- Coordi/Present/Feed/Main/VC/FeedViewModel.swift
+++ Coordi/Present/Feed/Main/VC/FeedViewModel.swift
@@ -14,14 +14,31 @@ final class FeedViewModel: ViewModelType {
     
     struct Input {
         let searchButtonTap: Observable<Void>
+        let temp: PublishRelay<Double>
     }
     
     struct Output {
         let serarchButtonTap: Driver<Void>
+        let temp: Driver<Int>
+        let tempText: Driver<String>
     }
     
     func transform(input: Input) -> Output {
+        let temp = PublishRelay<Int>()
+        let tempText = PublishRelay<String>()
+
+        input.temp
+            .map { Int($0) }
+            .bind(to: temp)
+            .disposed(by: disposeBag)
         
-        return Output.init(serarchButtonTap: input.searchButtonTap.asDriver(onErrorJustReturn: ()))
+        input.temp
+            .map { "현재 \(Int($0))℃" }
+            .bind(to: tempText)
+            .disposed(by: disposeBag)
+        
+        return Output.init(serarchButtonTap: input.searchButtonTap.asDriver(onErrorJustReturn: ()),
+                           temp: temp.asDriver(onErrorJustReturn: 0),
+                           tempText: tempText.asDriver(onErrorJustReturn: ""))
     }
 }

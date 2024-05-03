5519f4f81203f60e2c47602b46252cb42991c68a
chasomin <somin49@naver.com>
2024-05-03 19:32:25 +0900

    Feat: 작성일 ~ 현재 기간 반환하는 메서드 구현

--- Coordi/Extension/String+.swift
+++ Coordi/Extension/String+.swift
@@ -16,4 +16,29 @@ extension String {
         format.dateFormat = "yyyy년 M월 d일"
         return format.string(from: date)
     }
+    
+    func timeFormatString() -> Self {
+        let formatToDate = DateFormatter()
+        formatToDate.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        guard let date = formatToDate.date(from: self) else { return "" }
+        
+        
+        let offsetComps = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: date, to: Date())
+        
+        guard let second = offsetComps.second else { return ""}
+        guard let minute = offsetComps.minute else { return "" }
+        guard let hour = offsetComps.hour else { return "" }
+        guard let day = offsetComps.day else { return "" }
+        
+        if day == 0 && hour == 0 && minute == 0 {
+            return "\(second)초 전"
+        } else if day == 0 && hour == 0 {
+            return "\(minute)분 전"
+        } else if day == 0 {
+            return "\(hour)시간 전"
+        } else {
+            return "\(day)일 전"
+        }
+    }
+
 }

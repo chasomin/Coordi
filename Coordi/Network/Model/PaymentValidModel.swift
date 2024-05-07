0a1e0e002adc15e2cf48943c34799d14cd262c84
chasomin <somin49@naver.com>
2024-05-07 15:38:40 +0900

    Fix: 결제 유효성 확인 Model 변경

--- Coordi/Network/Model/PaymentValidModel.swift
+++ Coordi/Network/Model/PaymentValidModel.swift
@@ -8,8 +8,11 @@
 import Foundation
 
 struct PaymentValidModel: Decodable {
-    let imp_uid: String
+    let payment_id: String
+    let buyer_id: String
     let post_id: String
+    let merchant_uid: String
     let productName: String
     let price: Int
+    let paidAt: String
 }

4d8ed7f0dfc3b01be444ec4038d3a5cbe88b1f54
chasomin <somin49@naver.com>
2024-05-16 20:12:25 +0900

    Feat: ChatDetailViewController 생성

---
+++ Coordi/Present/Chat/ChatDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ChatDetailViewController.swift
+//  Coordi
+//
+//  Created by 차소민 on 5/16/24.
+//
+
+import UIKit
+
+class ChatDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    override func bind() {
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        navigationItem.title = Constants.NavigationTitle.chat.title
+    }
+
+}

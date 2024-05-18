2125e9d82d71569ed2763c0f516d54816d670db5
chasomin <somin49@naver.com>
2024-05-18 23:57:55 +0900

    Feat: 채팅ViewController UI 구성

--- Coordi/Present/Chat/ChatDetailViewController.swift
+++ Coordi/Present/Chat/ChatDetailViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 class ChatDetailViewController: BaseViewController {
+    let tableView = UITableView()
+    let sendStack = UIStackView()
+    let textField = RoundedTextFieldView()
+    let sendButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +33,39 @@ class ChatDetailViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        
+        view.addSubview(tableView)
+        view.addSubview(sendStack)
+        sendStack.addArrangedSubview(textField)
+        sendStack.addArrangedSubview(sendButton)
     }
     
     override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
         
+        sendStack.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(15)
+            make.height.equalTo(40)
+        }
+        
+        sendButton.snp.makeConstraints { make in
+            make.width.equalTo(sendButton.snp.height)
+        }
     }
     
     override func configureView() {
         navigationItem.title = Constants.NavigationTitle.chat.title
+        
+        tableView.separatorStyle = .none
+        sendStack.axis = .horizontal
+        sendStack.spacing = 10
+        sendStack.distribution = .fill
+        
+        let image = UIImage(systemName: "arrow.up.circle.fill")?.setConfiguration(font: .boldSystemFont(ofSize: 30))
+        sendButton.setImage(image, for: .normal)
+        textField.textField.placeholder = Constants.Placeholder.chat.rawValue
     }
 
 }

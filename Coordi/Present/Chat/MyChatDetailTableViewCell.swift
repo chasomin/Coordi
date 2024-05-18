22c9e701f93b5021524d919399d62a5a9b4bcd8b
chasomin <somin49@naver.com>
2024-05-19 01:26:11 +0900

    Feat: 내 채팅 cell 구현

---
+++ Coordi/Present/Chat/MyChatDetailTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  MyChatDetailTableViewCell.swift
+//  Coordi
+//
+//  Created by 차소민 on 5/20/24.
+//
+
+import UIKit
+import SnapKit
+
+class MyChatDetailTableViewCell: BaseTableViewCell {
+    let contentBubble = ChatBubble(isMyBubble: true)
+    let date = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(contentBubble)
+        contentView.addSubview(date)
+    }
+    
+    override func configureLayout() {
+        contentBubble.snp.makeConstraints { make in
+            make.verticalEdges.trailing.equalTo(contentView).inset(15)
+        }
+        
+        date.snp.makeConstraints { make in
+            make.trailing.equalTo(contentBubble.snp.leading)
+            make.bottom.equalTo(contentBubble)
+        }
+    }
+    
+    override func configureView() {
+        date.font = .caption
+        date.textColor = .lightGray
+        
+    }
+}

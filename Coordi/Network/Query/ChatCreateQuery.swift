8455cc35f7bd205963bf318151a38069e3e0f4cc
chasomin <somin49@naver.com>
2024-05-26 23:39:30 +0900

    Feat: 채팅방 생성 Query 구성

---
+++ Coordi/Network/Query/ChatCreateQuery.swift
@@ -0,0 +1,12 @@
+//
+//  ChatCreateQuery.swift
+//  Coordi
+//
+//  Created by 차소민 on 5/26/24.
+//
+
+import Foundation
+
+struct ChatCreateQuery: Encodable {
+    let opponent_id: String  // 채팅 상대방 user_id
+}
